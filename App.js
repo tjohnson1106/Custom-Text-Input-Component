ecd83417bf172337cdbbe2eafd6813ff808448cb
Thomas Johnson <tjohnson1106@gmail.com>
2018-08-10 15:45:36 -0400

    created text input scaffolding

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import MinMaxTextInput from "./src/components/MinMaxTextInput";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MinMaxTextInput />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 50
+  }
+});
---
+++ src/components/MinMaxTextInput.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class MinMaxTextInput extends Component {
+  state = {};
+  render() {
+    return (
+      <View>
+        <Text>Min Max</Text>
+      </View>
+    );
+  }
+}
+
+export default MinMaxTextInput;
